babcecd0f68d317a01046a0e57618450126d83be
wang-yichun <wang-yichun@meyougames.com>
2015-05-14 22:05:20 +0800

    update

---
+++ Assets/Scripts/RootController/Correlation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Correlation : MonoBehaviour {
+
+	public static Correlation getInstance ()
+	{
+		return GameObject.Find ("Controller").GetComponent<Correlation> ();
+	}
+
+
+}
